15d357974f097060eaa20f150ee162f1e6f4adf1
Pradhyo Bijja <pradhyo@nyu.edu>
2015-01-15 13:22:32 +0530

    Add function to return next day (assume 30 days per month)

---
+++ dates_difference.py
@@ -0,0 +1,13 @@
+# Program to find difference between two dates
+
+def nextDay(year, month, day):
+    """Simple version: assume every month has 30 days"""
+    if day < 30:
+        return year, month, day + 1
+    else:
+        if month == 12:
+            return year + 1, 1, 1
+        else:
+            return year, month + 1, 1
+
+print nextDay(2012,1,30)
