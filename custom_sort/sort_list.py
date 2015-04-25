57da42a6de891d5ce6392e23ef3223a79f22f07a
Pradhyo Bijja <pradhyo@nyu.edu>
2015-04-24 20:09:34 -0400

    Exit program with message when input file doesn't exist

--- custom_sort/sort_list.py
+++ custom_sort/sort_list.py
@@ -8,12 +8,16 @@ delchars = ''.join(c for c in map(chr, range(256)) if not c.isalnum() and c != '
 input_file_name = sys.argv[1] # second argument from command line
 output_file_name = sys.argv[2] # third argument from command line
 
-with open(input_file_name, 'rb') as input_file:
-	# Build list of items after removing special characters
-	list_items = input_file.read().translate(None, delchars).split(' ')
+try:
+	with open(input_file_name, 'rb') as input_file:
+		# Build list of items after removing special characters
+		list_items = input_file.read().translate(None, delchars).split(' ')
 
-print list_items
-	
-with open(output_file_name, 'wb') as output_file:
-	# Join list elements with space in between and write to output_file
-	output_file.write(' '.join(list_items))
+	print list_items
+		
+	with open(output_file_name, 'wb') as output_file:
+		# Join list elements with space in between and write to output_file
+		output_file.write(' '.join(list_items))
+
+except IOError:
+	print "No such input file, stopped program"
